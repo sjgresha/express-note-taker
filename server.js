0cc23070e3885aa1dca3982bad7faf50f003baf6
Steven Gresham <sbjg2545@gmail.com>
2023-11-06 15:52:28 -0700

    added routes

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
