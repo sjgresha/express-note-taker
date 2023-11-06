1a72c6c8853690d408e75da8fb179b04d9779799
Steven Gresham <sbjg2545@gmail.com>
2023-11-06 15:26:33 -0700

    added server.js and .gitignore

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+})
